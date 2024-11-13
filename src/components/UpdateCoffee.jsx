47d7311de4794b596d62a6804717049fcd772def
MohammadOfficials <mohammadofficial4@gmail.com>
2024-11-13 17:02:10 +0600

    coffee-store-first-commit

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">This is a Update Coffee page</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
